e60a5af1afa0516b6056e833ae3ba320216c3bc9
tryoma <tryoma0326@gmail.com>
2020-09-07 21:24:26 +0900

    user_情報編集

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,5 +1,7 @@
 class StaffsController < ApplicationController
+  
   def index
+    @staffs = Staff.all
   end
 
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+    # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+    def current_user?(user)
+      user == current_user
+    end
+    
 end
