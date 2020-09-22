69617d9f778ce9ff398ef5c95c4035b5bd8492bf
suzuki-yoshiki <yoshisu04183@gmail.com>
2020-09-22 15:19:21 +0900

    アプリケーション修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
@@ -22,5 +24,32 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  def set_two_weeks
+    if params[:date].nil?
+      require 'date'
+      @first_day = Date.today
+      @last_day = (@first_day+13.day)
+    else
+      @first_day = params[:date].to_date
+      @last_day = (@first_day+13.day)
+    end
+    @two_weeks = [*@first_day..@last_day] # 対象の週の日数を代入
+    @phone_reservations = PhoneReservation.where(worked_on: @first_day..@last_day)
+    unless @two_weeks.count == @phone_reservations.count
+      ActiveRecord::Base.transaction do
+        @two_weeks.each { |day| PhoneReservation.create!(worked_on: day) }  
+      end
+       @phone_reservations = PhoneReservation.where(worked_on: @first_day..@last_day)  
+    end
+
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
+      redirect_to root_url
+  end
+
+  def set_day_time
+    @day_time = [ "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00" ]
   end
 end
