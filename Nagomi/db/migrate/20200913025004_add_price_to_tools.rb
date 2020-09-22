c5d6bcba72d8f3b998dc4f9972aa4877b86845a0
suzuki-yoshiki <yoshisu04183@gmail.com>
2020-09-22 22:56:19 +0900

    edit-work2

--- Nagomi/db/migrate/20200913025004_add_price_to_tools.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToTools < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tools, :price, :integer
-  end
-end
--- Nagomi/db/migrate/20200914054451_add_content_price_to_works.rb
+++
@@ -1,5 +0,0 @@
-class AddContentPriceToWorks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :works, :content_price, :string
-  end
-end
