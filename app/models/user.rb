5868590b58883d56312aa3c04d39802fa2f0d963
masafumikazama <masanomaildesu@yahoo.co.jp>
2020-10-08 10:27:53 +1000

    作業内容を作成できるように再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :work_reservations, dependent: :destroy
 
-  validates :name, presence: true,length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :kana,length: { maximum: 50 }
   validates :sex,length: { maximum: 10 }
-  validates :email,length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 100 }, uniqueness: true
   validates :line_id, length: { maximum: 1000 }
   validates :address, length: { maximum: 1000 }
   validates :phone_number, length: { maximum: 1000 }
