fa97a57d0eb80f6bf26da939ccaf643bc72a707a
suzuki-yoshiki <yoshisu04183@gmail.com>
2020-10-11 19:45:46 +0900

    デプロイ先でエラーのため修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 # coding: utf-8
 #管理者ユーザー
 Staff.create!(name: "Sample User",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password",
-             admin: true)
+  email: "sample@email.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: true)
 
 #サンプルスタッフ            
 Staff.create!(name: "Sample Staff",
-              email: "sample-staff@email.com",
-              password: "password",
-              password_confirmation: "password")
+   email: "sample-staff@email.com",
+   password: "password",
+   password_confirmation: "password")
 
 puts "Staff Created"
 
 #サンプルユーザー             
 User.create!(name: "Sample-1 User",
-             email: "sample-1@email.com",
-             password: "password",
-             password_confirmation: "password",
-             admin: false)
+  email: "sample-1@email.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false)
 
 puts "User Created"
 
@@ -34,18 +34,18 @@ puts "User Created"
 
 # WorkReservation.create!(main_menu: "キッチンクリーニング",
 #                         option_menu: "電化製品")
-  
+
 # WorkReservation.create!(main_menu: "風呂場",
 #                         option_menu: "棚づくり")
-  
+
 # WorkReservation.create!(option_menu: "玄関")
 
 # WorkReservation.create!(option_menu: "トイレ")
-    
+
 # WorkReservation.create!(option_menu: "洗面所")
-    
+
 # WorkReservation.create!(option_menu: "庭")
-  
+
 
 # puts "WorkReservation Created"
 
@@ -53,4 +53,3 @@ puts "User Created"
 Map.create!(address: "静岡駅")
 
 puts "Map Created"
-
