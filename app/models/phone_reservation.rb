c0654492f027bc72af60f09fb8f84770b2ba0863
suzuki-yoshiki <yoshisu04183@gmail.com>
2020-10-17 19:01:09 +0900

    エラー修正

--- app/models/phone_reservation.rb
+++ app/models/phone_reservation.rb
@@ -1,5 +1,4 @@
 class PhoneReservation < ApplicationRecord
-  belongs_to :user
   validates :reservation_work, length: { maximum: 500 }
   enum  line_time: { "11:00": 0, "12:00": 1, "13:00": 2, "14:00": 3, "15:00": 4, "16:00": 5, "17:00": 6,}
 end
