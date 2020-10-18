ff30377e34426b7c0527f0375a3e5fd729fabd45
Tadakatsu Kato <tarako86226@gmail.com>
2020-10-18 23:10:59 +0900

    見積りカート完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :work_reservations, dependent: :destroy
   has_many :work_histories, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :kana,length: { maximum: 50 }
