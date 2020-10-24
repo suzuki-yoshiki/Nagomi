b0040ba9111ae8e2a01dbb6a36aa5d861ef721c1
suzuki-yoshiki <yoshisu04183@gmail.com>
2020-10-24 15:56:41 +0900

    修正

--- app/models/work_reservation.rb
+++ app/models/work_reservation.rb
@@ -1,5 +1,4 @@
 class WorkReservation < ApplicationRecord
-  belongs_to :user
   enum  main_menu: { "ー部屋掃除8畳以上" => 0, "ー部屋掃除6畳以下"=> 1, "レンジフードクリーニング"=> 2, "キッチンクリーニング"=> 3, "風呂場"=> 4}
 
   validates :main_menu, presence: true, allow_blank: true
